938ce29420d6d01f997f9c3baae84019e335dec5
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2020-05-30 13:30:23 +0200

    added some mdx styles for now 💅

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,21 +5,37 @@ import Header from './Header';
 
 const components = {
 	p: ({ children }) => (
-		<p className="text-base sm:text-lg text-gray-700 font-light mt-6">
+		<p className="mb-6 sm:mb-8 text-lg sm:text-xl leading-relaxed font-light text-gray-800">
 			{children}
 		</p>
+	),
+	h1: ({ children }) => (
+		<h1 className="mt-2 mb-4 text-4xl font-normal text-gray-900">
+			{children}
+		</h1>
+	),
+	h2: ({ children }) => (
+		<h2 className="mt-16 mb-4 text-3xl font-medium text-indigo-700">
+			{children}
+		</h2>
+	),
+	h3: ({ children }) => (
+		<h3 className="mt-8 mb-3 text-2xl font-medium text-gray-900">
+			{children}
+		</h3>
+	),
+	a: ({ children }) => (
+		<a className="font-normal underline text-indigo-600">{children}</a>
 	),
 };
 
 export default function Layout({ children }) {
 	return (
-		<div className="antialiased">
-			<div className="max-w-3xl mx-auto px-6">
-				<MDXProvider components={components}>
-					<Header />
-					{children}
-				</MDXProvider>
-			</div>
+		<div className="max-w-3xl mx-auto px-6">
+			<MDXProvider components={components}>
+				<Header />
+				{children}
+			</MDXProvider>
 		</div>
 	);
 }
