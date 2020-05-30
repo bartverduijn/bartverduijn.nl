f98403a54fe8e5bd9d48cdffa10959a163645367
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2020-05-30 13:44:35 +0200

    added mdxRenderer to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import useSiteMetaData from '../hooks/useSiteMetaData';
 import SEO from '../components/SEO';
-import Dump from '../components/Dump';
 
-export const TEST_CONTENT_QUERY = graphql`
-	query TEST_CONTENT_QUERY {
-		allMdx {
-			nodes {
-				id
-				excerpt(pruneLength: 250)
-				frontmatter {
-					title
-					date
-					slug
-					published
-				}
-				fields {
-					slug
-				}
+export const GET_HI_QUERY = graphql`
+	query GET_HI_QUERY {
+		mdx(fields: { slug: { eq: "/hi/" } }) {
+			body
+			frontmatter {
+				date(formatString: "DD/MM/YYYY")
 			}
 		}
 	}
@@ -26,58 +17,12 @@ export const TEST_CONTENT_QUERY = graphql`
 
 export default function IndexPage({ data }) {
 	const { title, description, author } = useSiteMetaData();
+	const { body } = data.mdx;
 	return (
 		<>
 			<SEO title="Home" />
-			<Dump data={data} />
-			{data.allMdx.nodes.map(({ frontmatter, fields }) => (
-				<p>
-					<Link to={fields.slug}>
-						<p>{frontmatter.title}</p>
-					</Link>
-				</p>
-			))}
-			<main className="text-base sm:text-lg text-gray-700 font-light">
-				<p className="mt-10">
-					Ik ben Bart. Ik ben 25 jaar oud, en op dit moment ben ik
-					werkzaam als Functioneel Applicatie Manager bij DAF Trucks
-					N.V.. Toen ik 2 jaar geleden deze website maakte, was ik net
-					een paar maanden in dienst bij Brunel. Ik had net mijn
-					eerste (korte) opdracht voltooid, en daarom zat ik tussen
-					twee opdrachten in op de zogenaamde 'bank', op zoek naar
-					mijn volgende uitdaging.
-				</p>
-				<p className="mt-6 sm:mt-8 md:mt-10">
-					Sindsdien is er veel veranderd, ben ik veel veranderd. Ik
-					ben weer heel wat certificeringen verder, en ik heb
-					ondertussen weer een stukje meer ervaring in het werkveld
-					dan dat ik twee jaar geleden had. Maar niet alleen op
-					professioneel gebied is alles anders. Ik woon op nu mezelf,
-					ik draag opeens witte overhemden naar mijn werk (mits ik
-					niet in thuisquarantaine zit), ik lust opeens koffie (wat
-					onhandig is met diezelfde witte overhemden) en ik heb zelfs
-					mijn vriendin aan de haak weten te slaan. Wat niet veranderd
-					is, is dat ik mezelf nog regelmatig de vraag stel die ik
-					destijds ook had: Wat wil ik eigenlijk worden als ik later
-					groot ben?
-				</p>
-				<p className="mt-6 sm:mt-8 md:mt-10">
-					Nu zal ik die vraag anders beantwoorden dan dat ik twee jaar
-					geleden deed. Mijn antwoord? Ik word bruggenbouwer. Ik wil
-					graag bruggen bouwen tussen mensen onderling. Ik wil met ze
-					meedenken en ze zelf het gevoel geven dat we samen tot een
-					mooi eindproduct kunnen komen. En ik wil bruggen bouwen
-					tussen mens en technologie. Hoe kan ik met mijn kennis
-					ervoor zorgen dat jouw werk net een stukje makkelijker
-					wordt, en jouw dag net een beetje leuker. Als ik later groot
-					ben, word ik bruggenbouwer.
-				</p>
-				<a
-					className="inline-block mt-10 py-2 px-6 border border-gray-700 rounded hover:bg-gray-700 hover:text-white"
-					href="mailto:bart.verduijn@gmail.com"
-				>
-					Mail Me
-				</a>
+			<main className="mt-12">
+				<MDXRenderer>{body}</MDXRenderer>
 			</main>
 		</>
 	);
