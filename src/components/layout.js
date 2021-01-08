aaf22cf921da5817883d40db261c0d3f89310508
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2021-01-08 14:26:26 +0100

    Add new design 💅

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-import { MDXProvider } from '@mdx-js/react';
 import 'normalize.css';
-import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
-import mdxComponents from './mdx';
-
-const theme = {
-	marginBottom: '2.16612rem',
-	fontSize: '18px',
-	grey900: 'hsl(209, 61%, 16%)',
-	grey800: 'hsl(211, 39%, 23%)',
-	grey700: 'hsl(209, 34%, 30%)',
-	grey600: 'hsl(209, 28%, 39%)',
-	grey500: 'hsl(210, 22%, 49%)',
-	grey400: 'hsl(209, 23%, 60%)',
-	grey300: 'hsl(211, 27%, 70%)',
-	grey200: 'hsl(210, 31%, 80%)',
-	grey100: 'hsl(212, 33%, 89%)',
-	grey000: 'hsl(210, 36%, 96%)',
-	blue900: 'hsl(245, 100%, 27%)',
-	blue700: 'hsl(245, 79%, 52%)',
-	blue500: 'hsl(243, 94%, 66%)',
-};
 
 const GlobalStyles = createGlobalStyle`
-	html {
-		font-size: ${theme.fontSize};
-		line-height: 1.625;
-		font-family: 'system-ui', -apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
-		box-sizing: border-box;
-		overflow-y: scroll;
+	@font-face {
+		font-family: 'Inter';
+		font-style:  normal;
+		font-weight: 400;
+		font-display: swap;
+		src: url('../fonts/Inter-Regular.woff2') format("woff2"),
+			url('../fonts/Inter-Regular.woff') format("woff");
 	}
+
+	@font-face {
+		font-family: 'Inter';
+		font-style:  normal;
+		font-weight: 700;
+		font-display: swap;
+		src: url('../fonts/Inter-Bold.woff2') format("woff2"),
+			url('../fonts/Inter-Bold.woff') format("woff");
+	}
+
+	@font-face {
+		font-family: 'Inter';
+		font-style:  normal;
+		font-weight: 800;
+		font-display: swap;
+		src: url('../fonts/Inter-ExtraBold.woff2') format("woff2"),
+			url('../fonts/Inter-ExtraBold.woff') format("woff");
+	}
+
+	@font-face {
+		font-family: 'Playfair Display';
+		font-style:  normal;
+		font-weight: 900;
+		font-display: swap;
+		src: url('../fonts/PlayfairDisplaySC-Black.woff2') format('woff2'),
+			url('../fonts/PlayfairDisplaySC-Black.woff') format('woff');
+	}
+
+	html {
+		font-size: 16px;
+		box-sizing: border-box;
+		--color-background: #FFFFFF;
+		--color-primary: #6366F1;
+		--color-gray-800: #1E293B;
+		--color-gray-400: #94A3B8;
+		--color-gray-300: #CBD5E1;
+	}
+
 	*, *:before, *:after {
-        box-sizing: inherit;
-    }
+		box-sizing: inherit;
+	}
+
 	body {
-		color: ${theme.grey700};
-		font-weight: 300;
-		word-wrap: break-word;
-		font-kerning: normal;
-		font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+		padding: 0;
+		margin: 0;
+		font-family: 'Inter', i-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+		border-top: 8px solid var(--color-primary);
 	}
 `;
 
 const ContainerStyles = styled.div`
-	max-width: 48rem;
+	max-width: 1280px;
 	margin: 0 auto;
 	padding: 0 1.5rem;
 `;
@@ -58,14 +77,11 @@ export default function Layout({ children, title }) {
 			<Helmet>
 				<title>{title}</title>
 			</Helmet>
-			<ThemeProvider theme={theme}>
-				<MDXProvider components={mdxComponents}>
-					<ContainerStyles>
-						<Header />
-						{children}
-					</ContainerStyles>
-				</MDXProvider>
-			</ThemeProvider>
+
+			<ContainerStyles>
+				<Header />
+				{children}
+			</ContainerStyles>
 		</>
 	);
 }
