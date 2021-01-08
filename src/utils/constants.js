bd73e02d2c0fbd3ec6fd4ac993e5a1ae0c8ffa66
bartverduijn <23431875+bartverduijn@users.noreply.github.com>
2021-01-08 16:16:39 +0100

    cleanup-after-each has become the default

---
+++ src/utils/constants.js
@@ -0,0 +1,28 @@
+export const COLORS = {
+
+}
+
+export const UNIT = 8;
+
+export const FONT_WEIGHTS = {
+    regular: 400,
+    bold: 500,
+    extraBold: 700,
+    black: 900,
+}
+
+export const BREAKPOINT_SIZES = {
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+    xxl: 1536
+}
+
+export const BREAKPOINTS = {
+    sm: `(min-width: ${BREAKPOINT_SIZES.sm}px)`,
+    md: `(min-width: ${BREAKPOINT_SIZES.md}px)`,
+    lg: `(min-width: ${BREAKPOINT_SIZES.lg}px)`,
+    xl: `(min-width: ${BREAKPOINT_SIZES.xl}px)`,
+    xxl: `(min-width: ${BREAKPOINT_SIZES.xxl}px)`,
+}
